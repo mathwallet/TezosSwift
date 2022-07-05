c81053ff150655427331e509d86996371b3ac280
xueyuejie <358838943@qq.com>
2022-07-05 14:43:47 +0800

    Update TezosOperationUtil.swift

--- Sources/TezosSwift/Transaction/Operation/TezosOperationUtil.swift
+++ Sources/TezosSwift/Transaction/Operation/TezosOperationUtil.swift
@@ -7,12 +7,40 @@
 
 import Foundation
 import BeaconBlockchainTezos
+import UIKit
 
 typealias TezosArg = Micheline.MichelsonV1Expression
 typealias TezosPrim = Micheline.Prim
 typealias TezosParameters = Tezos.Operation.Parameters
 
 public struct TezosOperationUtil {
+    
+    static func createDappOperation(operation:Tezos.Operation,from:String,to:String,metadata:TezosBlockchainMetadata)  -> Tezos.Operation {
+        switch operation {
+        case let .transaction(content):
+            return .transaction(Tezos.Operation.Transaction(source: from, fee: "0", counter: "\(metadata.counter)", gasLimit: MAXGAS, storageLimit: content.storageLimit, amount: content.amount, destination: content.destination, parameters: content.parameters))
+        case let .endorsement(content):
+            return .endorsement(Tezos.Operation.Endorsement(level: content.level))
+        case let .seedNonceRevelation(content):
+            return .seedNonceRevelation(Tezos.Operation.SeedNonceRevelation(level: content.level, nonce: content.nonce))
+        case let .doubleEndorsementEvidence(content):
+            return .doubleEndorsementEvidence(Tezos.Operation.DoubleEndorsementEvidence(op1: content.op1, op2: content.op2))
+        case let .doubleBakingEvidence(content):
+            return .doubleBakingEvidence(Tezos.Operation.DoubleBakingEvidence(bh1: content.bh1, bh2: content.bh2))
+        case let .activateAccount(content):
+            return .activateAccount(Tezos.Operation.ActivateAccount(pkh: content.pkh, secret: content.secret))
+        case let .proposals(content):
+            return .proposals(Tezos.Operation.Proposals(period: content.period, proposals: content.proposals))
+        case let .ballot(content):
+            return .ballot(Tezos.Operation.Ballot(source: from, period: content.period, proposal: content.proposal, ballot: content.ballot))
+        case let .reveal(content):
+            return .reveal(Tezos.Operation.Reveal(source: from, fee: "0", counter: "\(metadata.counter)", gasLimit: MAXGAS, storageLimit: content.storageLimit, publicKey: content.publicKey))
+        case let .origination(content):
+            return .origination(Tezos.Operation.Origination(source: from, fee: "0", counter: "\(metadata.counter)", gasLimit: MAXGAS, storageLimit: content.storageLimit, balance: content.balance, delegate: content.delegate, script: content.script))
+        case let .delegation(content):
+            return .delegation(Tezos.Operation.Delegation(source: from, fee: "0", counter: "\(metadata.counter)", gasLimit: MAXGAS, storageLimit: content.storageLimit, delegate: content.delegate))
+        }
+    }
     
     static func createOperation(from:String,to:String,mint:String = "",counter:String,amount:String,tokenType:TezosTransactionType,tokenId: String,metadata:TezosBlockchainMetadata)  -> Tezos.Operation {
         switch tokenType {
