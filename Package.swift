290569c713e3a324016a6563ac576b5da1246b59
xueyuejie <358838943@qq.com>
2022-07-08 16:15:56 +0800

    fix

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.4.1"),
         .package(name:"BIP39swift", url: "https://github.com/mathwallet/BIP39swift", from: "1.0.1"),
         .package(name: "Secp256k1Swift", url: "https://github.com/mathwallet/Secp256k1Swift.git", from: "1.2.5"),
-        .package(name: "BeaconSDK", url: "https://github.com/xueyuejie/beacon-ios-sdk.git", from: "3.1.2")
+        .package(name: "BeaconSDK", url: "https://github.com/xueyuejie/beacon-ios-sdk.git", from: "3.1.3")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
