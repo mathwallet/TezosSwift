4e100e0112b97cba051f9c642362537ad8d735e4
xueyuejie <358838943@qq.com>
2022-07-15 18:23:06 +0800

    Update ResponseMetadata.swift

--- Sources/TezosSwift/Provider/Response/ResponseMetadata.swift
+++ Sources/TezosSwift/Provider/Response/ResponseMetadata.swift
@@ -2,9 +2,9 @@
 import Foundation
 
 struct ResponseMetadata {
-    let operation_result: OperationResult?
-    let balance_updates:[OperationResultBalanceUpdates]
-    let internal_operation_results: [InternalOperationResult]
+    let operation_result: OperationResult
+    let balance_updates:[OperationResultBalanceUpdates]?
+    let internal_operation_results: [InternalOperationResult]?
 }
 
 extension ResponseMetadata: Decodable {
