c5926e31cdf231d9aa3d91d50a91c0847b515f47
xueyuejie <358838943@qq.com>
2022-07-15 18:51:06 +0800

    Update ResponseMetadata.swift

--- Sources/TezosSwift/Provider/Response/ResponseMetadata.swift
+++ Sources/TezosSwift/Provider/Response/ResponseMetadata.swift
@@ -6,18 +6,3 @@ struct ResponseMetadata {
     let balance_updates:[OperationResultBalanceUpdates]?
     let internal_operation_results: [InternalOperationResult]?
 }
-
-extension ResponseMetadata: Decodable {
-    private enum CodingKeys: String, CodingKey {
-        case operation_result
-        case balance_updates
-        case internal_operation_results
-    }
-    
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.operation_result = try container.decode(OperationResult.self, forKey: .operation_result)
-        self.balance_updates = try container.decodeIfPresent([OperationResultBalanceUpdates].self, forKey:.balance_updates ) ?? []
-        self.internal_operation_results = try container.decodeIfPresent([InternalOperationResult].self, forKey: .internal_operation_results) ?? []
-    }
-}
