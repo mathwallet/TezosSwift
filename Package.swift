444afd7398ffee4f917995c611078947bdb241ba
Forrest <zyyu1990@gmail.com>
2022-07-15 23:10:55 +0800

    Remove redundant dependent packages

--- Package.swift
+++ Package.swift
@@ -17,11 +17,11 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(name: "Base58Swift", url: "https://github.com/mathwallet/Base58Swift.git", from: "3.0.0"),
-        .package(name: "Sr25519", url: "https://github.com/lishuailibertine/Sr25519.swift.git", from: "0.1.6"),
         .package(name:"Blake2",url: "https://github.com/tesseract-one/Blake2.swift.git", from: "0.1.2"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.4.1"),
         .package(name:"BIP39swift", url: "https://github.com/mathwallet/BIP39swift", from: "1.0.1"),
         .package(url: "https://github.com/mxcl/PromiseKit.git", from: "6.8.4"),
+        .package(name: "TweetNacl", url: "https://github.com/lishuailibertine/tweetnacl-swiftwrap.git", from: "1.0.5")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -30,11 +30,11 @@ let package = Package(
             name: "TezosSwift",
             dependencies: [
                            "Base58Swift",
-                           .product(name: "Ed25519", package: "Sr25519"),
                            "Blake2",
                            "CryptoSwift",
                            "BIP39swift",
-                           "PromiseKit"
+                           "PromiseKit",
+                           "TweetNacl"
                           ]),
         .testTarget(
             name: "TezosSwiftTests",
--- Sources/TezosSwift/TezosKeypair.swift
+++ Sources/TezosSwift/TezosKeypair.swift
@@ -1,63 +1,69 @@
 import Foundation
 import BIP39swift
-import Ed25519
+import TweetNacl
 import CryptoSwift
 import Base58Swift
 import Blake2
 
 public struct TezosPrefix {
-    public static let edsk:[UInt8] = [43, 246, 78, 7]
-    public static let tz1:[UInt8] = [6, 161, 159]
-    public static let edsig:[UInt8] = [9, 245, 205, 134, 18]
-    public static let sig:[UInt8] = [4, 130, 43]
+    public static let edsk: [UInt8] = [43, 246, 78, 7]
+    public static let tz1: [UInt8] = [6, 161, 159]
+    public static let edsig: [UInt8] = [9, 245, 205, 134, 18]
+    public static let sig: [UInt8] = [4, 130, 43]
+}
+
+public struct TezosAddress {
+    public static let SIZE: Int = 20
+    // PublicKey Hash
+    public let data: Data
+    
+    public var address: String {
+        let addressBytes = TezosPrefix.tz1 + data.bytes
+        return Base58.base58CheckEncode(addressBytes)
+    }
+    
+    public init?(_ address: String) {
+        guard let addressBytes = Base58.base58CheckDecode(address), addressBytes.count == TezosAddress.SIZE else {
+            return nil
+        }
+        self.data = Data(addressBytes[3..<addressBytes.endIndex])
+    }
+    
+    public init(_ data: Data) {
+        self.data = data
+    }
 }
 
 public struct TezosKeypair {
     public var mnemonics: String?
-    
     public var derivePath: String?
-    
     public var secretKey: Data
+    public var publicKey: Data
     
-    private var keyPair:Ed25519KeyPair?
+    public var address: TezosAddress {
+        let publicKeyHash = try! publicKey.genericHash(outputLength: TezosAddress.SIZE)
+        return TezosAddress(publicKeyHash)
+    }
     
     public var privateKey: String {
         return Base58.base58CheckEncode(TezosPrefix.edsk + secretKey.bytes)
     }
     
-    public var publicKey: Data {
-        return self.keyPair!.publicKey.raw
-    }
-    
-    public var publicKeyHash: Data? {
-        guard let hashedData = self.genericHash(messageData: publicKey, outputLength: 20)else {
-            return nil
-        }
-        return hashedData
-    }
-    
-    public var address:String {
-        guard let _publicKeyHash = publicKeyHash else {
-            return ""
-        }
-        return Self.publicKeyHashToAddress(publicKeyHash: _publicKeyHash)
-    }
-    
-    public init(secretKey: Data) {
+    public init(secretKey: Data) throws {
         self.secretKey = secretKey
-        self.keyPair = try! Ed25519KeyPair(raw:secretKey)
+        self.publicKey = try NaclSign.KeyPair.keyPair(fromSecretKey: secretKey).publicKey
     }
     
-    public init(privateKey:String) throws {
+    public init(privateKey: String) throws {
         guard let privateBytes = Base58.base58CheckDecode(privateKey) else {
             throw Error.invalidPrivateKey
         }
-        self.init(secretKey:Data(privateBytes[4..<privateBytes.endIndex]))
+        try self.init(secretKey:Data(privateBytes[4..<privateBytes.endIndex]))
     }
     
-    public init(seed: Data)throws {
-        let ed25519KeyPair = try Ed25519KeyPair(seed: Ed25519Seed(raw: seed))
-        self.init(secretKey: ed25519KeyPair.raw)
+    public init(seed: Data) throws {
+        let secretKey = try NaclSign.KeyPair.keyPair(fromSeed: seed).secretKey
+        try self.init(secretKey: secretKey)
     }
     
     public init(mnemonics: String, path: String = "m/44'/1729'/0'/0'") throws {
@@ -78,46 +84,23 @@ public struct TezosKeypair {
     }
     
     public static func ed25519DeriveKey(path: String, seed: Data) -> (key: Data, chainCode: Data) {
-        return Ed25519KeyPair.deriveKey(path: path, seed: seed)
+        return NaclSign.KeyPair.deriveKey(path: path, seed: seed)
     }
     
     public static func ed25519DeriveKey(path: String, key: Data, chainCode: Data) -> (key: Data, chainCode: Data) {
-        return Ed25519KeyPair.deriveKey(path: path, key: key, chainCode: chainCode)
-    }
-}
-
-extension TezosKeypair {
-    
-    public static func publicKeyHashToAddress(publicKeyHash:Data) -> String {
-        let addressBytes = TezosPrefix.tz1+publicKeyHash.bytes
-        return Base58.base58CheckEncode(addressBytes)
-    }
-    
-    public static func addressToPublicKeyHash(address:String) -> Data? {
-        guard let addressBytes = Base58.base58CheckDecode(address) else {
-            return nil
-        }
-        return Data(addressBytes[3..<addressBytes.endIndex])
+        return NaclSign.KeyPair.deriveKey(path: path, key: key, chainCode: chainCode)
     }
 }
 
 // MARK: - Sign&Verify
 
 extension TezosKeypair {
-    public func signDigest(messageDigest:Data) -> Data {
-        return try! Ed25519KeyPair(raw:self.secretKey).sign(message: messageDigest).raw
+    public func signDigest(messageDigest: Data) throws -> Data {
+        return try NaclSign.sign(message: messageDigest, secretKey: secretKey)
     }
     
     public func verifyPublickey(message: Data, signature: Data) -> Bool {
-        return try! Ed25519KeyPair(raw:self.secretKey).verify(message: message, signature: Ed25519Signature(raw: signature))
-    }
-    
-    public func genericHash(messageData:Data,outputLength:Int) -> Data? {
-        let messageBytes = messageData.bytes
-        guard let hash = try? Blake2.hash(.b2b, size: outputLength, bytes: messageBytes) else {
-            return nil
-        }
-        return hash
+        return (try? NaclSign.signDetachedVerify(message: message, sig: signature, publicKey: publicKey)) ?? false
     }
 }
 
