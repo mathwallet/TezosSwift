9ffd8409ea9a9005954d16d6673ddf4c3f2d3eb8
xueyuejie <358838943@qq.com>
2022-07-15 14:37:44 +0800

    Update OperationStatus.swift

--- Sources/TezosSwift/Provider/Response/OperationStatus.swift
+++ Sources/TezosSwift/Provider/Response/OperationStatus.swift
@@ -13,22 +13,23 @@ struct OperationStatus:Codable {
     let metadata: ResponseMetadata?
 }
 
-extension OperationStatus {
-    private enum CodingKeys: String, CodingKey {
-        case counter
-        case metadata
-    }
-
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.kind = try container.decode(String.self, forKey: .counter)
-        self.source = try container.decode(String.self, forKey: .counter)
-        self.fee = try container.decode(String.self, forKey: .counter)
-        self.counter = try container.decode(String.self, forKey: .counter)
-        self.gas_limit = try container.decode(String.self, forKey: .counter)
-        self.storage_limit = try container.decode(String.self, forKey: .counter)
-        self.amount = try container.decode(String.self, forKey: .counter)
-        self.destination = try container.decode(String.self, forKey: .counter)
-        self.metadata = try container.decode(ResponseMetadata.self, forKey: .metadata)
-    }
-}
+//extension OperationStatus {
+//    private enum CodingKeys: String, CodingKey {
+//        case counter
+//        case metadata
+//        case
+//    }
+//
+//    init(from decoder: Decoder) throws {
+//        let container = try decoder.container(keyedBy: CodingKeys.self)
+//        self.kind = try container.decode(String.self, forKey: .counter)
+//        self.source = try container.decode(String.self, forKey: .counter)
+//        self.fee = try container.decode(String.self, forKey: .counter)
+//        self.counter = try container.decode(String.self, forKey: .counter)
+//        self.gas_limit = try container.decode(String.self, forKey: .counter)
+//        self.storage_limit = try container.decode(String.self, forKey: .counter)
+//        self.amount = try container.decode(String.self, forKey: .counter)
+//        self.destination = try container.decode(String.self, forKey: .counter)
+//        self.metadata = try container.decode(ResponseMetadata.self, forKey: .metadata)
+//    }
+//}
