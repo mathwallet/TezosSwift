6df421139b2dafdc5e2a6bc27057868dd5683e0d
Forrest <zyyu1990@gmail.com>
2024-05-30 18:15:04 +0800

    update

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         .package(name: "Blake2", url: "https://github.com/lishuailibertine/Blake2.swift", from: "0.1.3"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.4.1"),
         .package(name:"BIP39swift", url: "https://github.com/mathwallet/BIP39swift", from: "1.0.1"),
-        .package(url: "https://github.com/mxcl/PromiseKit.git", from: "6.8.4"),
+        .package(url: "https://github.com/mxcl/PromiseKit.git", from: "8.1.1"),
         .package(name: "TweetNacl", url: "https://github.com/lishuailibertine/tweetnacl-swiftwrap.git", from: "1.0.5")
     ],
     targets: [
